6ee67d0cfa104ac44cdc6610cedd808bf2205d31
Duc Luu <ducluu84@yahoo.com>
2013-07-08 07:28:21 +0000

    Modify web.js again

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
    var fs = require('fs');
-   var con = fs.readFileSync('/index.html', function (err, data) {
+   var con = fs.readFileSync('index.html', function (err, data) {
        if (err) throw err;
        console.log(data);
        });
