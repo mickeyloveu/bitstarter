f6b1df303980a168fdd839f51c0918c4fd2b9817
Duc Luu <ducluu84@yahoo.com>
2013-07-08 07:14:41 +0000

    Modify web.js again

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+   var con = fs.readFileSync('./index.html', function (err, data) {
+       if (err) throw err;
+       console.log(data);
+       });
+   response.send(con);
 });
 
 var port = process.env.PORT || 5000;
